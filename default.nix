dbb9336b0aaa47546c1448e69d925ad82bb98356
Peter Jones <pjones@devalot.com>
2019-10-14 09:17:47 -0700

    Update dependencies (zxcvbn-hs, lens)

--- default.nix
+++ default.nix
@@ -4,22 +4,24 @@
 let
   nix-hs-src = fetchGit {
     url = "https://code.devalot.com/open/nix-hs.git";
-    rev = "2003332a1e8e518b54e6143f9a9467a8a05abca4";
+    rev = "a2b666faf8cb3c6f769655dfb36f4695f78bc3c3";
   };
 
   nix-hs = import "${nix-hs-src}/default.nix" { inherit pkgs; };
-
-  zxcvbn-hs-src = {
-    url = "https://code.devalot.com/sthenauth/zxcvbn-hs.git";
-    rev = "07a224794f0299e4c066056841485e2d86d8b075";
-  };
-
-  zxcvbn-hs = import "${fetchGit zxcvbn-hs-src}/default.nix" { inherit pkgs; };
 
 in nix-hs {
   cabal = ./zxcvbn-dvorak.cabal;
 
   overrides = lib: self: super: with lib; {
-    zxcvbn-hs = zxcvbn-hs;
+    zxcvbn-hs = lib.fetchGit {
+      url = "https://code.devalot.com/sthenauth/zxcvbn-hs.git";
+      rev = "5c522f929e7aff38cb477160ec89cf854cc1ecca";
+      ref = "next";
+    };
+
+    lens =
+      if super ? lens_4_18
+        then super.lens_4_18
+        else super.lens;
   };
 }
