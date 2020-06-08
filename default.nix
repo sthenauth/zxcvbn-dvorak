c3c23a5d8d57c533ec112b9c2d0d0dd27d17325e
Peter Jones <pjones@devalot.com>
2020-06-08 13:41:27 -0700

    Upgrade nix-hs

--- default.nix
+++ default.nix
@@ -1,27 +1,16 @@
-{ sources ? import ./nix/sources.nix
-, pkgs ? import sources.nixpkgs { }
+{ sources ? import ./nix/sources.nix, pkgs ? import sources.nixpkgs { }
 , nix-hs ? import sources.nix-hs { inherit pkgs; }
-, zxcvbn-hs ? sources.zxcvbn-hs
-, ghcide ? sources.ghcide-nix
-, ormolu ? sources.ormolu
-, ghc ? "default"
-}:
+, zxcvbn-hs ? sources.zxcvbn-hs, ghc ? "default" }:
 
 nix-hs {
   cabal = ./zxcvbn-dvorak.cabal;
   compiler = ghc;
 
-  overrides = lib: self: super: with lib; {
-    zxcvbn-hs = import zxcvbn-hs {
-      inherit (lib) pkgs;
-      inherit ghc;
+  overrides = lib: self: super:
+    with lib; {
+      zxcvbn-hs = import zxcvbn-hs {
+        inherit (lib) pkgs;
+        inherit nix-hs ghc;
+      };
     };
-
-    ghcide = import ghcide {};
-
-    ormolu = (import ormolu {
-      inherit (lib) pkgs;
-      ormoluCompiler = lib.compilerName;
-    }).ormolu;
-  };
 }
